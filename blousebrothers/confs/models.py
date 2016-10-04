b0e66827d7fe36e7579f83fe893c7381e732509c
Julien Almarcha <julien.almarcha@gmail.com>
2016-10-04 11:31:58 +0200

    image url enhancement and deletion bugfix fix #35

--- blousebrothers/confs/models.py
+++ blousebrothers/confs/models.py
@@ -1,8 +1,5 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
-import os
 
-from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
@@ -28,8 +25,8 @@ class Conference(models.Model):
     slug = AutoSlugField(_('Slug'), max_length=128, unique=True,
                          populate_from='title')
     summary = models.CharField(_('Esprit du dossier'), blank=False, null=False, max_length=140,
-                               help_text=_('Ex: "dossier très pointu et monothématique sur la fibrillation auriculaire" '
-                                           'ou "dossier transversal de révisions classiques sur lupus et grossesse" '))
+                               help_text=_('Ex: "dossier très pointu et monothématique sur la fibrillation auriculaire"'
+                                           ' ou "dossier transversal de révisions classiques sur lupus et grossesse" '))
     statement = models.TextField(_('Énoncé*'), blank=True, null=True)
     items = models.ManyToManyField('Item', verbose_name=("Items"), related_name='conferences',
                                    help_text=_('Ne sélectionnez que les items abordés de manière '
@@ -42,24 +39,27 @@ class Conference(models.Model):
         return reverse('confs:update', kwargs={'slug': self.slug})
 
 
+def conf_directory_path(conf_image, filename):
+    return '{0}/conf_{1}/{2}'.format(conf_image.conf.owner.username,
+                                conf_image.conf.id,
+                                filename)
+
 
 class ConferenceImage(models.Model):
-    image = models.ImageField(_("Image"), upload_to=settings.OSCAR_IMAGE_FOLDER, max_length=255)
+    image = models.ImageField(_("Image"), upload_to=conf_directory_path, max_length=255)
     date_created = models.DateTimeField(_("Date created"), auto_now_add=True)
     caption = models.CharField(_("Légende"), max_length=200, blank=True)
     index = models.PositiveIntegerField(_("Ordre"), default=0)
     conf = models.ForeignKey('Conference', related_name='images')
 
 
-# These two auto-delete files from filesystem when they are unneeded:
-@receiver(models.signals.post_delete, sender=ConferenceImage)
-def auto_delete_file_on_delete(sender, instance, **kwargs):
+@receiver(models.signals.pre_delete, sender=ConferenceImage)
+def auto_delete_conference_image_on_delete(sender, instance, **kwargs):
     """Deletes file from filesystem
     when corresponding `MediaFile` object is deleted.
     """
     if instance.image:
-        if os.path.isfile(instance.image.path):
-            os.remove(instance.image.path)
+        instance.image.delete()
 
 
 class Item(models.Model):
@@ -88,23 +88,54 @@ class Question(models.Model):
 
     def is_valid(self):
         one_good = len([a for a in self.answers.all() if a.answer and a.correct]) >= 1
-        all_filled = len([a for a in self.answers.all() if a.answer ]) == 5
+        all_filled = len([a for a in self.answers.all() if a.answer]) == 5
         return one_good and all_filled
+
+
+def answer_image_directory_path(answer_image, filename):
+    return '{0}/conf_{1}/answers/{2}'.format(answer_image.question.conf.owner.username,
+                                answer_image.question.conf.id,
+                                filename)
 
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, related_name="answers")
     answer = models.TextField(_("Proposition"), blank=True, null=True)
     explaination = models.TextField(_("Explication"), blank=True, null=True)
-    explaination_image = models.ImageField(_("Image"), upload_to=settings.OSCAR_IMAGE_FOLDER, max_length=255,
+    explaination_image = models.ImageField(_("Image"), upload_to=answer_image_directory_path, max_length=255,
                                            blank=True, null=True)
     correct = models.BooleanField(_("Correct"), default=False)
     ziw = models.BooleanField(_("Zéro si erreur"), default=False)
     index = models.PositiveIntegerField(_("Ordre"), default=0)
 
+@receiver(models.signals.pre_delete, sender=Answer)
+def auto_delete_answer_image_on_delete(sender, instance, **kwargs):
+    """Deletes file from filesystem
+    when corresponding `MediaFile` object is deleted.
+    """
+    if instance.explaination_image:
+        instance.explaination_image.delete()
+
+
+def question_image_directory_path(question_image, filename):
+    return '{0}/conf_{1}/questions/{2}'.format(question_image.question.conf.owner.username,
+                                question_image.question.conf.id,
+                                filename)
+
+
 class QuestionImage(models.Model):
-    image = models.ImageField(_("Image"), upload_to=settings.OSCAR_IMAGE_FOLDER, max_length=255)
+    image = models.ImageField(_("Image"), upload_to=question_image_directory_path, max_length=255)
     date_created = models.DateTimeField(_("Date created"), auto_now_add=True)
     caption = models.CharField(_("Libellé"), max_length=200, blank=True)
     index = models.PositiveIntegerField(_("Ordre"), default=0)
     question = models.ForeignKey('Question', related_name='images')
+
+
+@receiver(models.signals.pre_delete, sender=QuestionImage)
+def auto_delete_question_image_on_delete(sender, instance, **kwargs):
+    """Deletes file from filesystem
+    when corresponding `MediaFile` object is deleted.
+    """
+    if instance.image:
+        instance.image.delete()
+
