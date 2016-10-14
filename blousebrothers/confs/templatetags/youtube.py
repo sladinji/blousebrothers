170c74b00249c774b0895d0dc30a3341bfc621b4
Julien Almarcha <julien.almarcha@gmail.com>
2016-10-15 01:35:18 +0200

    remove print...

--- blousebrothers/confs/templatetags/youtube.py
+++ blousebrothers/confs/templatetags/youtube.py
@@ -6,7 +6,6 @@ register = template.Library()
 @register.filter(name='youtube')
 def youtube(value):
     if 'youtube' in value:
-        print(value)
         value = re.sub(r'<img class="ta-insert-video" src="[^"]*" ta-insert-video="([^"]*)" allowfullscreen="true"[^>]*>',
                        r'<iframe width="420" height="315" src="\1" allowfullscreen="true"></iframe>',
                         value)
