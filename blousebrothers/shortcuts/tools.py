a35b9b4a0eb8963649f61d6e55e780713f62e813
Julien Almarcha <julien.almarcha@gmail.com>
2016-10-18 13:47:21 +0200

    edition interface enhancement fix #76 fix #78

--- blousebrothers/shortcuts/tools.py
+++ blousebrothers/shortcuts/tools.py
@@ -1,4 +1,4 @@
-from django import template
+from django.forms.models import model_to_dict
 
 def analyse_conf(conf):
     total_q = len(conf.questions.all())
@@ -8,9 +8,11 @@ def analyse_conf(conf):
     progress = len(valid_q) / total_q * 100
     conf.edition_progress = int(progress)
     conf.save()
+    confdict = model_to_dict(conf)
+    confdict.update(pk=conf.id)# to be compatible with djng rest api
     return {
         'total_q': total_q,
         'written_q': [q.id for q in written_q],
         'valid_q': [q.id for q in valid_q],
-        'progress': progress,
+        'conference': confdict,
     }
