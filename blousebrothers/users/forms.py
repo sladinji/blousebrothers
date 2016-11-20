b1ce73bd4a7dca21805889080880c71606db32a1
Julien Almarcha <julien.almarcha@gmail.com>
2016-11-20 10:56:25 +0100

    add users.forms

---
+++ blousebrothers/users/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from oscar.forms.widgets import DatePickerInput
+
+from .models import User
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'address1', 'address2', 'zip_code',
+                'city', 'email', 'university', 'degree', 'mobile',
+                ]
+
+
+class WalletForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['birth_date', 'country_of_residence', 'nationality']
+    birth_date = forms.DateField(widget=DatePickerInput)
