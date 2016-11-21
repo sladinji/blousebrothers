bc6324d422643f80ef483a989910bfe28819a07d
Julien Almarcha <julien.almarcha@gmail.com>
2016-11-21 11:02:36 +0100

    add migration script for conferencier permission fix #132

---
+++ blousebrothers/users/migrations/0006_add_permission_to_conferenciers.py
@@ -0,0 +1,20 @@
+from django.db import migrations, models
+
+def add_permission(apps, schema_editor):
+    User = apps.get_model('users', 'User')
+    Permission = apps.get_model('auth', 'Permission')
+    perm = Permission.objects.get(name='Can add conference')
+    for user in User.objects.filter(is_conferencier=True).exclude(user_permissions=perm):
+        user.user_permissions.add(perm)
+        user.save()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0005_auto_20161120_0940'),
+    ]
+
+    operations = [
+        # omit reverse_code=... if you don't want the migration to be reversible.
+        migrations.RunPython(add_permission),
+    ]
