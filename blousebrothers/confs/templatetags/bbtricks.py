f6d400c2288206e9401fd3d6ab9fc74949ef60a1
Julien Almarcha <julien.almarcha@gmail.com>
2016-12-05 14:23:39 +0100

    Update test result UI (striped background, wired but tell me what you think ;)

--- blousebrothers/confs/templatetags/bbtricks.py
+++ blousebrothers/confs/templatetags/bbtricks.py
@@ -1,5 +1,6 @@
 from string import ascii_uppercase
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -8,9 +9,23 @@ def to_char(value):
     return ascii_uppercase[value]
 
 @register.filter
-def is_good(answer, test_answer):
+def is_good_css(answer, test_answer):
     if answer.correct and str(answer.index) in test_answer.given_answers or\
     not answer.correct and str(answer.index) not in test_answer.given_answers :
         return "correct"
     else:
-        return "notcorrect"
+        return "fatal" if answer.ziw else "notcorrect"
+
+@register.filter
+def zero_cause_error_label(answer, test_answer):
+    if is_good_css(answer, test_answer) == "fatal":
+        return "(zéro sur cette mauvaise réponse) "
+    else:
+        return ""
+
+@register.filter
+def get_checked_fa(answer, test_answer):
+    if str(answer.index) in test_answer.given_answers :
+        return mark_safe('<i class="fa fa-check-square-o" aria-hidden="true"></i>')
+    else :
+        return mark_safe('<i class="fa fa-square-o" aria-hidden="true"></i>')
