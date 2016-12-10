87b41931fac6181f96216eb4be278fd6b2eb36fc
Julien Almarcha <julien.almarcha@gmail.com>
2016-12-10 12:48:32 +0100

    bug fix youtube display

--- blousebrothers/confs/templatetags/youtube.py
+++ blousebrothers/confs/templatetags/youtube.py
@@ -5,7 +5,7 @@ register = template.Library()
 
 @register.filter(name='youtube')
 def youtube(value):
-    if 'youtube' in value:
+    if value and 'youtube' in value:
         value = re.sub(r'<img class="ta-insert-video" src="[^"]*" ta-insert-video="([^"]*)" allowfullscreen="true"[^>]*>',
                        r'<iframe width="420" height="315" src="\1" allowfullscreen="true"></iframe>',
                         value)
