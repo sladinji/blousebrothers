bbc602d58b3c9d12dc62dea1c45811f346b6a3b4
Julien Almarcha <julien.almarcha@gmail.com>
2017-01-24 09:30:09 +0100

    affichage 0 par défaut sur l'interface liste conf + CSS

---
+++ blousebrothers/confs/management/commands/cgu_sync.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from django.contrib.flatpages.models import FlatPage
+from termsandconditions.models import TermsAndConditions
+
+
+class Command(BaseCommand):
+    help = 'Sync cgu accept content wit /CGU'
+
+
+    def handle(self, *args, **options):
+        cgu = TermsAndConditions.objects.last()
+        cgu.text = FlatPage.objects.get(title='CGU').content
+        cgu.save()
