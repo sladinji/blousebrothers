985b82b5fceca6ca5a36d3a61d1fc918006b11fa
Julien Almarcha <julien.almarcha@gmail.com>
2017-01-30 15:54:55 +0100

    fix googlebot issue with user that need to be imported from blousebrothers.users

---
+++ googlebot/middleware.py
@@ -0,0 +1,29 @@
+import socket
+
+from django.contrib.auth.models import AnonymousUser
+from blousebrothers.users.models import User
+
+
+class GooglebotMiddleware(object):
+    """
+    Middleware to automatically log in the Googlebot with the user account 'googlebot'
+    """
+    def process_request(self, request):
+        request.is_googlebot = False # Assume false, until proven
+        if request.user == AnonymousUser():
+            if request.META.get('HTTP_USER_AGENT'):
+                if 'Googlebot' in request.META['HTTP_USER_AGENT']:
+                    try:
+                        remote_ip = request.META['REMOTE_ADDR']
+                        hostname = socket.gethostbyaddr(remote_ip)[0]
+
+                        if hostname.endswith('googlebot.com'):
+                            request.user, created = User.objects.get_or_create(username='googlebot') # login our googlebot user :)
+                            request.is_googlebot = True
+                        else:
+                            # FAKE googlebot!!!!
+                            request.is_googlebot = False
+
+                    except Exception:
+                        pass # Don't bring down the site
+        return None
