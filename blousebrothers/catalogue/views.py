d16e77fbc76e91fa5b9c88ff25df0208e4c1a879
Julien Almarcha <julien.almarcha@gmail.com>
2017-02-07 14:42:19 +0100

    check for bonus on catalogue

--- blousebrothers/catalogue/views.py
+++ blousebrothers/catalogue/views.py
@@ -1,4 +1,7 @@
 from oscar.apps.catalogue.views import ProductDetailView as CoreProductDetailView
+from oscar.apps.catalogue.views import CatalogueView as CoreCatalogueView
+
+from blousebrothers.tools import check_bonus
 
 
 class ProductDetailView(CoreProductDetailView):
@@ -6,6 +9,13 @@ class ProductDetailView(CoreProductDetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         obj = self.get_object()
-        if obj.conf :
+        if obj.conf:
             context['meta'] = obj.conf.as_meta(self.request)
         return context
+
+
+class CatalogueView(CoreCatalogueView):
+
+    def get(self, *args, **kwargs):
+        check_bonus(self.request, self.request.user)
+        return super().get(*args, **kwargs)
