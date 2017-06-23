5fe9ae6ce09f40035d4562ee560c36e8bc40b9a3
Julien Almarcha <julien.almarcha@gmail.com>
2017-06-23 16:51:51 +0200

    move raven (sentry spam)

--- blousebrothers/confs/management/commands/mailsync.py
+++ blousebrothers/confs/management/commands/mailsync.py
@@ -1,9 +1,6 @@
 from django.core.management.base import BaseCommand
 from blousebrothers import mailchimp
 from blousebrothers.users.models import User
-from raven import Client
-
-sentry = Client('https://770aeeaa5cc24a3e8b16a10c328c28c5:1aca22596ba1421198ff5269032f0ffd@sentry.io/104798')
 
 
 class Command(BaseCommand):
@@ -14,6 +11,8 @@ class Command(BaseCommand):
         parser.add_argument('username', nargs='*')
 
     def handle(self, *args, **options):
+        from raven import Client
+        sentry = Client('https://770aeeaa5cc24a3e8b16a10c328c28c5:1aca22596ba1421198ff5269032f0ffd@sentry.io/104798')
         try:
             if options['username']:
                 qs = User.objects.filter(username__in=options['username']).all()
