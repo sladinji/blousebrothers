902b51102cbb83889b5179c7132b0b645b461de0
Julien Almarcha <julien.almarcha@gmail.com>
2017-06-25 12:32:53 +0200

    remove D4 offer

--- blousebrothers/users/urls.py
+++ blousebrothers/users/urls.py
@@ -71,15 +71,15 @@ urlpatterns = [
         name='invitation'
     ),
     # SpecialOffer demand
-    url(
-        regex=r'^~specialoffer/$',
-        view=views.SpecialOffer.as_view(),
-        name='specialoffer'
-    ),
+    #url(
+    #    regex=r'^~specialoffer/$',
+    #    view=views.SpecialOffer.as_view(),
+    #    name='specialoffer'
+    #),
     # ActivateOffer demand
-    url(
-        regex=r'^~activate/(?P<user_id>[\d]*)$',
-        view=views.ActivateOffer.as_view(),
-        name='activateoffer'
-    ),
+    #url(
+    #    regex=r'^~activate/(?P<user_id>[\d]*)$',
+    #    view=views.ActivateOffer.as_view(),
+    #    name='activateoffer'
+    #),
 ]
