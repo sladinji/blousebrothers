341fcaffe75cdaa12bb9f5e049b9f1e561b90878
Julien Almarcha <julien.almarcha@gmail.com>
2017-07-11 14:44:39 +0200

    fix unit test

--- blousebrothers/users/migrations/0006_add_permission_to_conferenciers.py
+++ blousebrothers/users/migrations/0006_add_permission_to_conferenciers.py
@@ -1,12 +1,17 @@
-from django.db import migrations, models
+from django.db import migrations
+
 
 def add_permission(apps, schema_editor):
     User = apps.get_model('users', 'User')
     Permission = apps.get_model('auth', 'Permission')
-    perm = Permission.objects.get(name='Can add conference')
-    for user in User.objects.filter(is_conferencier=True).exclude(user_permissions=perm):
-        user.user_permissions.add(perm)
-        user.save()
+    try:
+        perm = Permission.objects.get(name='Can add conference')
+        for user in User.objects.filter(is_conferencier=True).exclude(user_permissions=perm):
+            user.user_permissions.add(perm)
+            user.save()
+    except:
+        pass
+
 
 class Migration(migrations.Migration):
 
