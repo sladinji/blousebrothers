e7ebf45b4039077019a7e3cf69c7ac2f5756a431
simon <simonchollethan@gmail.com>
2017-08-18 17:56:00 +0200

    revision_steps.py, difficulty curve

--- blousebrothers/cards/models.py
+++ blousebrothers/cards/models.py
@@ -1,4 +1,4 @@
-from datetime import timedelta
+from datetime import timedelta, datetime
 from django.db import models
 from django.dispatch import receiver
 from django.db.models.signals import pre_save
@@ -31,7 +31,7 @@ class Deck(models.Model):
     difficulty = models.PositiveIntegerField(_("Difficulté"), choices=DIFFICULTY_CHOICES, default=1)
     created = models.DateTimeField(auto_now_add=True)
     column = models.PositiveIntegerField(default=0)
-    wake_up = models.DateTimeField()
+    wake_up = models.DateTimeField(default=datetime(2000, 1, 1))
     modified = models.DateTimeField(auto_now=True)
     nb_views = models.IntegerField(default=0)
 
