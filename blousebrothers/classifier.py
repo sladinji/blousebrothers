a3623c40466b7c74aa6ece4dc6d2751f8569bfce
Julien Almarcha <julien.almarcha@gmail.com>
2017-08-23 10:13:31 +0200

    fix classifier not available

--- blousebrothers/classifier.py
+++ blousebrothers/classifier.py
@@ -1,18 +1,22 @@
-from blousebrothers.confs.models import Classifier
+import logging
 
 from sklearn.externals import joblib
-from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.linear_model import SGDClassifier
+
+from blousebrothers.confs.models import Classifier
 
 __classifier = None
+
+logger = logging.getLogger(__name__)
 
 
 def classifier(string):
     global __classifier
-    if not __classifier:
-        __classifier = joblib.load(Classifier.objects.first().classifier)
+    try:
+        if not __classifier:
+            __classifier = joblib.load(Classifier.objects.first().classifier)
+    except:
+        logger.exception("No classifier available")
+        return []
     prediction = __classifier.predict_proba([string])
-    classement = sorted(zip(prediction[0], __classifier.classes_), reverse = True)
+    classement = sorted(zip(prediction[0], __classifier.classes_), reverse=True)
     return [i[1] for i in classement[:3]]
