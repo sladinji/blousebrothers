b2c53bb79084d617e766d2b5c3301c575c7c95f1
Julien Almarcha <julien.almarcha@gmail.com>
2017-08-29 23:16:49 +0200

    fix card admin

--- blousebrothers/cards/admin.py
+++ blousebrothers/cards/admin.py
@@ -6,10 +6,9 @@ from blousebrothers.cards.models import Card, Deck
 
 class CardAdmin(admin.ModelAdmin):
     list_display = ('content',)
-    list_filter = ('specialities', 'items',)
+    list_filter = ('specialities',)
     filter_horizontal = ['items', 'specialities']
     search_fields = ['content', ]
-    list_editable = ('content',)
 
 
 admin.site.register(Card, CardAdmin)
