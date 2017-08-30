60d3e6dfedf764b85f8df938924478199ee29cb2
Julien Almarcha <julien.almarcha@gmail.com>
2017-08-30 08:44:15 +0200

    add editable

--- blousebrothers/cards/admin.py
+++ blousebrothers/cards/admin.py
@@ -7,6 +7,7 @@ from blousebrothers.cards.models import Card, Deck
 class CardAdmin(admin.ModelAdmin):
     list_display = ('id', 'content',)
     list_filter = ('specialities',)
+    list_editable = ("content",)
     filter_horizontal = ['items', 'specialities']
     search_fields = ['content', ]
 
