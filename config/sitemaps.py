9f2cca3a0078a21e516728c20e40b287fb967f1a
Julien Almarcha <julien.almarcha@gmail.com>
2017-09-07 23:00:45 +0200

    update sitemap

--- config/sitemaps.py
+++ config/sitemaps.py
@@ -2,25 +2,29 @@ from django.contrib import sitemaps
 from django.core.urlresolvers import reverse
 from django.apps import apps
 
-statics = ['home', '@@@FAQ/' , '@@@cgu/', '@@@mentionslegales/']
+from blousebrothers.cards.models import Card
+
+statics = ['home', '/FAQ/', '/cgu/', '/mentionslegales/']
 
 Product = apps.get_model('catalogue', 'Product')
+
 
 class StaticViewSitemap(sitemaps.Sitemap):
     priority = 0.5
     changefreq = 'daily'
 
     def items(self):
-        print("ahah")
         return statics + list(
-            Product.objects.all().values_list("slug", "pk")
+            Product.objects.all()
+        ) + list(
+            Card.objects.filter(public=True).all()
         )
 
     def location(self, item):
         if item not in statics:
-            return reverse('catalogue:detail', kwargs={'product_slug':item[0], 'pk': item[1]})
-        else :
-            if item.startswith("@@@"):
-                return item.replace("@@@","/")
-            else :
+            return item.get_absolute_url()
+        else:
+            if item.startswith("/"):
+                return item
+            else:
                 return reverse(item)
