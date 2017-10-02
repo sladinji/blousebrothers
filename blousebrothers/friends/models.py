54bd83caa4dfc0cf63b859f8473a3316656f0c99
Julien Almarcha <julien.almarcha@gmail.com>
2017-10-02 10:47:48 +0200

    friendship admin

--- blousebrothers/friends/models.py
+++ blousebrothers/friends/models.py
@@ -8,6 +8,11 @@ class FriendShipRequest(models.Model):
     accepted = models.BooleanField(default=False)
     deleted = models.BooleanField(default=False)
 
+    def __str__(self):
+        txt = "{0.requester} -> {0.target} ({0.create_timestamp}, accepted: {0.accepted},"
+        txt += "deleted: {0.deleted})"
+        return txt.format(self)
+
 
 class Relationship(models.Model):
     from_user = models.ForeignKey('users.User', related_name='gives_friendship')
@@ -17,6 +22,12 @@ class Relationship(models.Model):
     share_confs = models.BooleanField(default=True)
 
     def __repr__(self):
-        text = "<{0.from_user} --> {0.to_user}, cards : {0.share_cards}, "
-        text += "results : {0.share_results}, confs: {0.share_confs}>"
-        return text.format(self)
+        txt = "<{0.from_user} --> {0.to_user}, cards : {0.share_cards}, "
+        txt += "results : {0.share_results}, confs: {0.share_confs}>"
+        return txt.format(self)
+
+    def __str__(self):
+        txt = "{0.from_user} --> {0.to_user}, cards : {0.share_cards}, "
+        txt += "results : {0.share_results}, confs: {0.share_confs}"
+        return txt.format(self)
+
