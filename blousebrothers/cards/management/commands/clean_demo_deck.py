04f574827cd318a124ea4250e720dd1b4eccb258
Julien Almarcha <julien.almarcha@gmail.com>
2017-10-12 13:51:57 +0200

    Ajout de log sur clean demo deck

--- blousebrothers/cards/management/commands/clean_demo_deck.py
+++ blousebrothers/cards/management/commands/clean_demo_deck.py
@@ -9,8 +9,9 @@ class Command(BaseCommand):
     help = 'Remove demo deck cards older than 3 days'
 
     def handle(self, *args, **options):
-        User.objects.get(
+        deletions = User.objects.get(
             username="demo"
         ).deck.filter(
             created__lt=datetime.datetime.today() - datetime.timedelta(days=3)
         ).delete()
+        print("{} cards deleted".format(deletions))
