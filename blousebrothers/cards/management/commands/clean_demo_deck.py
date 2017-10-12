057a1eb163890cbe39e5b45828f754366c57476c
Julien Almarcha <julien.almarcha@gmail.com>
2017-10-12 13:49:46 +0200

    Ajout de clean deck demo

---
+++ blousebrothers/cards/management/commands/clean_demo_deck.py
@@ -0,0 +1,16 @@
+import datetime
+
+from django.core.management.base import BaseCommand
+
+from blousebrothers.users.models import User
+
+
+class Command(BaseCommand):
+    help = 'Remove demo deck cards older than 3 days'
+
+    def handle(self, *args, **options):
+        User.objects.get(
+            username="demo"
+        ).deck.filter(
+            created__lt=datetime.datetime.today() - datetime.timedelta(days=3)
+        ).delete()
